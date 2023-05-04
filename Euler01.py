ff55306c641f42d7dbee6d9e2e2a3b3a640459cc
Taylor Glaws <tglaws27@gmail.com>
2023-05-04 12:12:41 -0400

    Project Euler Problem 1

---
+++ Euler01.py
@@ -0,0 +1,12 @@
+def main():
+    total = 0
+    for i in range(0, 1000):
+        if i%3==0:
+                total += i
+        elif i%5==0:
+                total += i
+    print(total)
+
+main()
+
+
